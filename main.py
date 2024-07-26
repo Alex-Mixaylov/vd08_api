690adf756618db15de4573c147eeef766cf0edf9
Alex <guardamar81@gmail.com>
2024-07-26 20:48:17 +0400

    ok
    ok

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     weather = None
+    news = None
     error = None
     if request.method == 'POST':
         city = request.form['city']
         weather, error = get_weather(city)
-    return render_template("index.html", weather=weather, error=error)
+        news, error = get_news()
+    return render_template("index.html", weather=weather, news=news, error=error)
 
 def get_weather(city):
     api_key = "d910ee490c508d67d15d77ec547f829d"
@@ -20,6 +22,15 @@ def get_weather(city):
         return response.json(), None
     else:
         return None, "Город не найден или произошла ошибка."
+
+def get_news():
+    api_key = "02d610c099184e36a958fdf51270787e"
+    url = f"https://newsapi.org/v2/everything?q=travel&language=ru&apiKey={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json().get('articles', []), None
+    else:
+        return None, "Произошла ошибка при получении новостей."
 
 if __name__ == '__main__':
     app.run(debug=True)
